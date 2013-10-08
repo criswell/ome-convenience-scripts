f3412401fee5acb76c7cf17e49dc31d08f777bf0
Sam Hart <sam@glencoesoftware.com>
2013-10-07 21:12:43 -0400

    start support for arch

--- make_dev_chroot.sh
+++ make_dev_chroot.sh
@@ -20,15 +20,49 @@ fi
 
 DEST=$1
 
-debootstrap raring ${DEST}
-cp -f ${MY_PATH}/sources.list ${DEST}/etc/apt/sources.list
-cp -f ${MY_PATH}/deb_setup_env.sh ${DEST}/root/.
+error_wrongDistro() {
+cat <<EOF
+
+ This script must be run in a one of the following distributions:
+    * Debian-derived distribution (such as Ubuntu)
+    * Arch Linux
+
+ Your distribution does not appear to be supported.
+
+EOF
+}
+
+strap_deb() {
+    debootstrap raring ${DEST}
+    cp -f ${MY_PATH}/sources.list ${DEST}/etc/apt/sources.list
+    cp -f ${MY_PATH}/deb_setup_env.sh ${DEST}/root/setup_env.sh
+}
+
+strap_arch() {
+    mkdir -p ${DEST}
+    pacstrap -d ${DEST} bash python2-pip python2 pacman vim less
+    # We assume we want our parent's mirrorlist
+    cp -f /etc/pacman.d/mirrorlist ${DEST}/etc/pacman.d/mirrorlist
+    cp -f ${MY_PATH}/arch_setup_env.sh ${DEST}/root/setup_env.sh
+}
+
+if [ -f "/etc/debian_version" ]; then
+    strap_deb
+elif [ -f "/etc/arch-release" ]; then
+    strap_arch
+else
+    error_wrongDistro
+    exit 1
+fi
+
 cp -f ${MY_PATH}/init_env.sh ${DEST}/root/.
-chmod a+x ${DEST}/root/deb_setup_env.sh
+chmod a+x ${DEST}/root/setup_env.sh
 
-chroot ${DEST} /root/deb_setup_env.sh
+chroot ${DEST} /root/setup_env.sh
 
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo ">> Chroot setup. Run /root/init_env.sh once in chroot to"
-echo ">> initialize the development environment"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+cat <<EOF
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+>> Chroot setup. Run /root/init_env.sh once in chroot to
+>> initialize the development environment
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+EOF
