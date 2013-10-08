78a2b002bc9134a5ecb043887f988325af095bdd
Sam Hart <sam@glencoesoftware.com>
2013-10-07 22:35:04 -0400

    need some precursor for ice, also need as root option

--- arch_setup_env.sh
+++ arch_setup_env.sh
@@ -13,13 +13,19 @@ pacman-key --populate archlinux
 pacman-key --refresh-keys
 
 # Now the packages
-pacman -Syu --noconfirm jdk7-openjdk python2-scipy python2-numpy wget curl unzip git gcc
+pacman -Syu --noconfirm jdk7-openjdk python2-scipy python2-numpy wget \
+    curl unzip git gcc tar gzip
+
+curl -L https://aur.archlinux.org/packages/ja/java-berkeleydb/java-berkeleydb.tar.gz -o /root/java-berkeleydb.tar.gz
+tar xzvf java-berkeleydb.tar.gz
+cd java-berkeleydb
+makepkg --sign -s --noconfirm --asroot
 
 # Install ICE
 curl -L https://aur.archlinux.org/packages/ze/zeroc-ice/zeroc-ice.tar.gz -o /root/zeroc-ice.tar.gz
 tar xzvf zeroc-ice.tar.gz
 cd zeroc-ice
-makepkg --sign -s --noconfirm
+makepkg --sign -s --noconfirm --asroot
 
 
 # Final pip installs
