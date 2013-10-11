8269e45e75a911c9e0e50d0d5aa1933dee0fc43d
Sam Hart <sam@glencoesoftware.com>
2013-10-11 11:11:08 -0400

    missing deps and need JAVA_HOME for arch

--- arch_setup_env.sh
+++ arch_setup_env.sh
@@ -14,19 +14,26 @@ pacman-key --refresh-keys
 
 # Now the packages
 pacman -Syu --noconfirm jdk7-openjdk python2-scipy python2-numpy wget \
-    curl unzip git gcc tar gzip grep automake make
+    curl unzip git gcc tar gzip grep automake make glib2 file patch \
+    diffutils mcpp php python
+
+source /etc/profile
 
 curl -L https://aur.archlinux.org/packages/ja/java-berkeleydb/java-berkeleydb.tar.gz -o /root/java-berkeleydb.tar.gz
 tar xzvf java-berkeleydb.tar.gz
 cd java-berkeleydb
 makepkg --sign -s --noconfirm --asroot
+pacman -U --noconfirm java-berkeleydb-*.pkg.tar.xz
 
 # Install ICE
 curl -L https://aur.archlinux.org/packages/ze/zeroc-ice/zeroc-ice.tar.gz -o /root/zeroc-ice.tar.gz
 tar xzvf zeroc-ice.tar.gz
 cd zeroc-ice
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk/
 makepkg --sign -s --noconfirm --asroot
+pacman -U --noconfirm *.pkg.tar.xz
 
+pacman -Syu --noconfirm apache-ant postgresql
 
 # Final pip installs
 pip install numexpr
