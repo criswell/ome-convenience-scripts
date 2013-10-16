14b81ee71e52b90e32729d689623fefcbd5c9ef3
Sam Hart <sam@glencoesoftware.com>
2013-10-15 20:41:55 -0400

    update arch, delete bootstrap

--- arch_setup_env.sh
+++ arch_setup_env.sh
@@ -15,7 +15,9 @@ pacman-key --refresh-keys
 # Now the packages
 pacman -Syu --noconfirm jdk7-openjdk python2-scipy python2-numpy wget \
     curl unzip git gcc tar gzip grep automake make glib2 file patch \
-    diffutils mcpp php which
+    diffutils mcpp php which systemd python python2-virtualenv
+
+ln -s /usr/bin/python2.7 /usr/bin/python
 
 source /etc/profile
 
@@ -25,9 +27,12 @@ cd java-berkeleydb
 makepkg --sign -s --noconfirm --asroot
 pacman -U --noconfirm java-berkeleydb-*.pkg.tar.xz
 
+cd /root
+
 # Install ICE
 curl -L https://aur.archlinux.org/packages/ze/zeroc-ice/zeroc-ice.tar.gz -o /root/zeroc-ice.tar.gz
-tar xzvf zeroc-ice.tar.gz
+tar xzvf /root/zeroc-ice.tar.gz
+
 cd zeroc-ice
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk/
 makepkg --sign -s --noconfirm --asroot
@@ -35,6 +40,12 @@ pacman -U --noconfirm *.pkg.tar.xz
 
 pacman -Syu --noconfirm apache-ant postgresql
 
+virtualenv2 --system-site-packages -p /usr/bin/python2.7 /venv
+
 # Final pip-2.7 installs
 pip-2.7 install numexpr
 pip-2.7 install cython
+
+echo "##########################################"
+echo "### Be sure to source /venv/bin/activate"
+echo "##########################################"
